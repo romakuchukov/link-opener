304c524edd8e62b8fd137d86e2c243781a62bc16
Roma K <romankuchukov@gmail.com>
2020-01-24 00:34:44 -0500

    added styling though withStyles

--- src/cmp/linkLoader.js
+++ src/cmp/linkLoader.js
@@ -1,7 +1,40 @@
-import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+import { TextareaAutosize, withStyles } from '@material-ui/core';
 
-export default function MaxHeightTextarea() {
+
+const styles = (theme) => ({
+  stepper: {
+    padding: theme.spacing(3, 0, 5),
+    [theme.breakpoints.down(290)]: { display: 'none' }
+  },
+  buttons: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  button: {
+    marginTop: theme.spacing(3),
+    marginLeft: theme.spacing(1),
+  },
+  typography: { padding: theme.spacing(2) },
+});
+
+const MaxHeightTextarea = (props) => {
+  const {classes} = props;
+
+  const parseLinks = () => {
+    console.log('logging', 1);
+    console.log(classes), 2;
+    //console.log(styles, 3);
+  }
   return (
-    <TextareaAutosize rowsMax={4} aria-label="maximum height" placeholder="Maximum 4 rows" defaultValue="" />
+    <TextareaAutosize
+      onChange={parseLinks}
+      onFocus={parseLinks}
+      className={classes.button}
+      aria-label="Paste links"
+      placeholder="Paste your links"
+      defaultValue=""
+    />
   );
 }
+
+export default withStyles(styles)(MaxHeightTextarea);

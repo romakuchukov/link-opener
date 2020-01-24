f2d31ef00a2ddd80abd1909518fff814a39bf7b0
Roma K <romankuchukov@gmail.com>
2020-01-24 16:55:46 -0500

    added styles

--- src/cmp/linkLoader.js
+++ src/cmp/linkLoader.js
@@ -1,51 +1,66 @@
-import { TextareaAutosize } from '@material-ui/core';
+import { TextareaAutosize, withStyles } from '@material-ui/core';
 
-import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(theme => ({
-  appBar: { position: 'relative' },
-  layout: {
-    width: 'auto',
-    marginLeft: theme.spacing(2),
-    marginRight: theme.spacing(2),
-    [theme.breakpoints.up(800 + theme.spacing(2) * 2)]: {
-      width: '100%',
-      maxWidth: 800,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-    },
+const styles = (theme) => ({
+  common: {
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    padding: 0,
+    margin: 0
   },
-  paper: {
-    marginTop: theme.spacing(3),
-    marginBottom: theme.spacing(3),
-    padding: theme.spacing(2),
-    [theme.breakpoints.up(800 + theme.spacing(3) * 2)]: {
-      marginTop: theme.spacing(6),
-      marginBottom: theme.spacing(6),
-      padding: theme.spacing(3),
-    },
-  }
-}));
-//const styles = useStyles();
+  list: {
+    '& li': { listStyle: 'none' }
+  },
+  textArea: {
+    resize: 'none',
+    color:'#222',
+    border: 'none',
+    borderBottom: '1px solid #222',
+    fontWeight:'normal',
+    fontSize:20,
+    lineHeight:'1.3em',
+   },
+});
 
 const MaxHeightTextarea = (props) => {
-  const styles = useStyles();
+  const [state, setState] = React.useState('');
+  const {classes} = props;
 
-  const parseLinks = () => {
-    console.log('logging', 1);
-    console.log(styles), 2;
-    //console.log(styles, 3);
+  const parseLinks = (e) => {
+    setState(e.target.value);
   }
+
+  const mergeCls = (...classList) => classList.join(' ');
+
   return (
-    <TextareaAutosize
-      onChange={parseLinks}
-      onFocus={parseLinks}
-      className={styles.button}
-      aria-label="Paste links"
-      placeholder="Paste your links"
-      defaultValue=""
-    />
+    <React.Fragment>
+      {console.log(state.list)}
+      <TextareaAutosize
+        value={state.value}
+        onChange={(e) => parseLinks(e)}
+        className={mergeCls(classes.textArea, classes.common)}
+        rowsMax={20}
+        aria-label="Paste links"
+        placeholder="Paste your links"
+      />
+      <ul className={mergeCls(classes.list, classes.common)}>
+        {state && state.split("\n").filter(item => !!item).map((item,key) => <li key={key}><a href={item.trim()}>{item.trim()}</a></li>)}
+      </ul>
+    </React.Fragment>
   );
 }
 
-export default MaxHeightTextarea;
+export default withStyles(styles)(MaxHeightTextarea);
+/*
+
+ https://create-react-app.dev/docs/making-a-progressive-web-app/
+https://css-tricks.com/snippets/css/a-guide-to-flexbox/#flexbox-background
+https://www.tutorialrepublic.com/faq/how-to-disable-resizable-property-of-textarea-using-css.php
+https://www.w3schools.com/cssref/css3_pr_resize.asp
+https://www.w3docs.com/snippets/css/how-to-disable-the-resizing-of-the-textarea-element.html
+https://electrictoolbox.com/disable-textarea-resizing-safari-chrome/
+
+*/

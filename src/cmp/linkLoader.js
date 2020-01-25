a27a9d6449950ea0d52eeceb118e3c406c43c7c0
Roma K <romankuchukov@gmail.com>
2020-01-24 22:40:16 -0500

    styled the page

--- src/cmp/linkLoader.js
+++ src/cmp/linkLoader.js
@@ -1,28 +1,53 @@
 import { TextareaAutosize, withStyles } from '@material-ui/core';
 
+const typography = {
+  fontSize:20,
+  lineHeight:'1.3em',
+  fontWeight:'normal',
+  fontFamily: 'inherit',
+  letterSpacing: 'normal'
+};
 
 const styles = (theme) => ({
+  buttons: {
+    display: 'flex',
+    justifyContent: 'center',
+    '& > *': { margin: 'auto' },
+    '& button:first-of-type': { marginLeft: 0 },
+    '& button:last-of-type': { marginRight: 0 },
+  },
   common: {
+    background: '#fafafa',
     width: '100%',
     height: '100%',
-    position: 'absolute',
-    top: 0,
-    left: 0,
+    // top: 0,
+    // left: 0,
     padding: 0,
-    margin: 0
+    margin: 0,
+    zIndex: -1
   },
   list: {
-    '& li': { listStyle: 'none' }
+    zIndex: -1,
+    position: 'relative',
+    paddingBottom: 1,
+    outline: 'none!important',
+    '& li': { listStyle: 'none' },
+    '& a': { ...typography },
   },
+  textPane: { marginTop: '5%' },
   textArea: {
+    position: 'absolute',
     resize: 'none',
     color:'#222',
     border: 'none',
     borderBottom: '1px solid #222',
-    fontWeight:'normal',
-    fontSize:20,
-    lineHeight:'1.3em',
+    outline: 'none',
+    ...typography
    },
+   visible: {
+    zIndex: 1,
+    outline: 'solid 1px #e0e0e0'
+  }
 });
 
 const MaxHeightTextarea = (props) => {

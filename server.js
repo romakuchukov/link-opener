459d4f13776e4f0c2b2fe3d1403ee4045a701812
Roma K <romankuchukov@gmail.com>
2020-02-05 19:35:32 -0500

    added NODE_ENV

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
+
+if(process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
+  })
+}
+
 app.use(favicon(__dirname + '/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));

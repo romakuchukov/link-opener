8d4b569e88ab16fca44ec5cc364d2acaf58d6eac
Roma K <romankuchukov@gmail.com>
2020-02-06 11:27:54 -0500

    glob

--- webpack.config.js
+++ webpack.config.js
@@ -59,12 +59,11 @@ const config = {
     new GenerateSW({
       globDirectory: '.',
       globPatterns: [
-        'favicon.ico',
-        'index.html',
-        'logo192.png',
-        'logo512.png',
-        'manifest.json',
-        'public/*'
+        '/favicon.ico',
+        '/index.html',
+        '/logo192.png',
+        '/logo512.png',
+        '/manifest.json'
       ],
       // these options encourage the ServiceWorkers to get in there fast
       // and not allow any straggling "old" SWs to hang around
